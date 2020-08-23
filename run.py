f6df3cff976bcb92581f44dfbb479e879e222698
zizhazhu <zizhazhu@gmail.com>
2020-08-23 23:25:57 +0800

    fix bug

--- run.py
+++ run.py
@@ -12,7 +12,7 @@ class Net(torch.nn.Module):
         self.action_embed = torch.nn.Embedding(2, 64)
         self.state_trans = torch.nn.Linear(4, 64)
         self.fc2 = torch.nn.Linear(128, 128)
-        self.fc3 = torch.nn.Linear(128, 1)
+        self.output = torch.nn.Linear(128, 1)
 
     def forward(self, s, a):
         s = torch.tensor(s, dtype=torch.float)
@@ -21,15 +21,18 @@ class Net(torch.nn.Module):
         s_embed = F.relu(self.state_trans(s))
         sa = torch.cat((s_embed, a_embed), dim=1)
         sa = F.relu(self.fc2(sa))
-        q = self.fc3(sa)
+        q = self.output(sa)
         return q
 
 
 def get_action(s, net, verbose=False):
     import random
-    if random.uniform(0, 1) < 0.05:
-        return random.randint(0, 1)
     max_q, max_a = 1, -1
+    if random.uniform(0, 1) < 0.05:
+        max_a = random.randint(0, 1)
+        if verbose:
+            print('explore {}'.format(max_a))
+        return max_a
     for a in range(2):
         a_tensor = torch.tensor([a], dtype=torch.long)
         q = net(s, a_tensor)
@@ -44,6 +47,7 @@ def get_action(s, net, verbose=False):
 env = gym.make('CartPole-v0')
 env.reset()
 net = Net()
+gamma = 0.5
 
 criterion = nn.MSELoss()
 optimizer = torch.optim.Adam(net.parameters(), lr=0.001)
@@ -56,13 +60,13 @@ for epoch in range(300):
         action = get_action(ob, net, verbose=False)
         path.append([ob, [action], 0])
         ob, r, done, info = env.step(action)
+        env.render()
         path[-1][-1] = r
         ob = (ob,)
-        env.render()
     g = 0
     loss_value = 0
     for all_state in reversed(path):
-        g += all_state[2]
+        g = all_state[2] + gamma * g
         optimizer.zero_grad()
         outputs = net(all_state[0], all_state[1])
         g_tensor = torch.tensor([[g]], dtype=torch.float)
