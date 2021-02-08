679576b7f86d278c62b0e725a936d60f70040594
zizhazhu <zizhazhu@gmail.com>
2021-02-08 18:53:01 +0800

    finish vpg

--- src/rllib/demo/vpg.py
+++ src/rllib/demo/vpg.py
@@ -39,7 +39,7 @@ class Net(torch.nn.Module):
         return q
 
 
-def get_model(observation_space, mlp_layers=(32,), output_space=1):
+def get_policy_model(observation_space, mlp_layers=(32,), output_space=1):
     layers = []
     layers.append(nn.Embedding(observation_space, mlp_layers[0]))
     for i in range(1, len(mlp_layers)):
@@ -49,46 +49,78 @@ def get_model(observation_space, mlp_layers=(32,), output_space=1):
     return nn.Sequential(*layers)
 
 
+def get_value_model(observation_space, mlp_layers=(32,)):
+    layers = []
+    layers.append(nn.Embedding(observation_space, mlp_layers[0]))
+    for i in range(1, len(mlp_layers)):
+        layers.append(nn.Linear(mlp_layers[i-1], mlp_layers[i]))
+        layers.append(nn.ReLU())
+    layers.append(nn.Linear(mlp_layers[-1], 1))
+    return nn.Sequential(*layers)
+
+
 class VanillaPolicyGradient:
 
-    def __init__(self, model):
-        self._model = model
+    def __init__(self, a_model, v_model):
+        self._a_model = a_model
+        self._v_model = v_model
 
     def get_policy(self, obs):
-        logits = self._model(obs)
+        logits = self._a_model(obs)
         cate = Categorical(logits=logits)
         return cate
 
     def get_action(self, obs):
         return self.get_policy(obs).sample().item()
 
-    def compute_loss(self, obs, act, weights) -> torch.Tensor:
+    def get_value(self, obs):
+        value = self._v_model(obs)
+        return value
+
+    def compute_policy_loss(self, obs, act, weights) -> torch.Tensor:
         log_p = self.get_policy(obs).log_prob(act)
         # 因为优化器是梯度下降，所以取负
         weighted_logp = - log_p * weights
         return weighted_logp.mean()
 
+    def compute_value_loss(self, obs, returns):
+        values = self.get_value(obs)
+        loss = torch.nn.functional.mse_loss(values, returns)
+        return loss
 
-def train(epoch=1, gamma=1.0, render=False, model_path='./model/FrozenLake-v0/pg.model'):
+
+def train(epoch=1, gamma=1.0, render=False, model_path='./model/FrozenLake-v0/vpg.model'):
     env = gym.make('FrozenLake-v0')
 
     # iter common
-    model = get_model(
+    a_model = get_policy_model(
         env.observation_space.n,
         [32, 32],
         env.action_space.n,
     )
+    v_model = get_value_model(
+        env.observation_space.n,
+        [32, 32],
+    )
     if os.path.exists(model_path):
-        model.load_state_dict(torch.load(model_path))
-    agent = VanillaPolicyGradient(model)
-    optimizer = torch.optim.Adam(model.parameters())
+        state_dict = torch.load(model_path)
+        a_model.load_state_dict(state_dict['a'])
+        v_model.load_state_dict(state_dict['v'])
+    agent = VanillaPolicyGradient(a_model, v_model)
+    a_optimizer = torch.optim.Adam(a_model.parameters())
+    v_optimizer = torch.optim.Adam(v_model.parameters())
 
     def one_epoch():
+        # step 1: collect trajectories
+        # step 2: calculate return
+        # step 3: calculate Advantage estimate
+        # step 4: backward policy gradient
+        # step 5: backward value function
         batch_obs = []
-        batch_act = []
+        batch_acts = []
+        batch_rewards = []
+        batch_values = []
         batch_weights = []
-        tra_length = 0
-        g_return = 0
 
         obs, done = env.reset(), False
 
@@ -98,24 +130,40 @@ def train(epoch=1, gamma=1.0, render=False, model_path='./model/FrozenLake-v0/pg
                 env.render()
 
             batch_obs.append(obs)
+            value = agent.get_value(torch.as_tensor(obs, dtype=torch.long))
+            batch_values.append(value)
 
             action = agent.get_action(torch.as_tensor(obs, dtype=torch.long))
             obs, reward, done, _ = env.step(action)
-            tra_length += 1
-            g_return = reward + gamma * g_return
 
-            batch_act.append(action)
-        batch_weights.extend([g_return] * tra_length)
+            batch_rewards.append(reward)
+            batch_acts.append(action)
 
-        optimizer.zero_grad()
-        loss = agent.compute_loss(
+        batch_values.append(0)
+        for i in range(len(batch_rewards)):
+            batch_weights.append(batch_values[i+1] + batch_rewards[i] - batch_values[i])
+        batch_returns = [0 for _ in range(len(batch_rewards))]
+        batch_returns[-1] = batch_rewards[-1]
+        for i in reversed(range(len(batch_rewards) - 1)):
+            batch_returns[i] = batch_rewards[i] + gamma * batch_returns[i + 1]
+
+        a_optimizer.zero_grad()
+        v_optimizer.zero_grad()
+        policy_loss = agent.compute_policy_loss(
             obs=torch.as_tensor(batch_obs, dtype=torch.long),
-            act=torch.as_tensor(batch_act, dtype=torch.int32),
+            act=torch.as_tensor(batch_acts, dtype=torch.int32),
             weights=torch.as_tensor(batch_weights, dtype=torch.float32),
         )
-        loss.backward()
-        optimizer.step()
-        logging.info(f"Loss:{loss:.3} Return:{g_return} Length:{tra_length}")
+        policy_loss.backward()
+        value_loss = agent.compute_value_loss(
+            torch.as_tensor(batch_obs, dtype=torch.long),
+            torch.as_tensor(batch_returns, dtype=torch.float32),
+        )
+        value_loss.backward()
+        a_optimizer.step()
+        v_optimizer.step()
+        logging.info(f"Policy_loss:{policy_loss:.3} Value_loss:{value_loss:.3} Return:{batch_returns[0]} Length:{len(batch_returns)}")
+        logging.info("")
 
     for epoch_n in range(epoch):
         logging.info(f"Epoch {epoch_n}:")
@@ -125,9 +173,9 @@ def train(epoch=1, gamma=1.0, render=False, model_path='./model/FrozenLake-v0/pg
     directory, file = os.path.split(model_path)
     if not os.path.exists(directory):
         os.mkdir(directory)
-    torch.save(model.state_dict(), model_path)
+    torch.save({'a': a_model.state_dict(), 'v': v_model.state_dict()}, model_path)
 
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO)
-    train(gamma=0.99, epoch=5000, render=False)
+    train(gamma=0.99, epoch=3, render=True)
